0f15ff8ef7ecb700b4f8b482e523c42697c1bce5
Reddy9100 <129190164+Reddy9100@users.noreply.github.com>
2024-04-19 12:20:06 +0530

    Update index.js
    docker check

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.listen(3000,()=>{
     console.log("server is connected")
 })
 app.get("/", (req, res) => {
-  res.send("<h1>This is Deployed from Ec2</h1>")
+  res.send("<h1>This is Deployed from Ec2 and used CI/CD PipeLine</h1>")
   });
