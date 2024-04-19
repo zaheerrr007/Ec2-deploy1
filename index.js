3fa124ed323e2bd6fafdbde0dfce9663e7886824
Reddy9100 <129190164+Reddy9100@users.noreply.github.com>
2024-04-19 12:42:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.listen(3000,()=>{
     console.log("server is connected")
 })
 app.get("/", (req, res) => {
-  res.send("<h1>This is Deployed from Ec2 and used Docker</h1>")
+  res.send("<h1>This is Deployed from Ec2 and used Docker..</h1>")
   });
